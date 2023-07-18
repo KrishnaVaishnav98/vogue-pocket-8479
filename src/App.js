1b5acd0c8a1377e8df8c8a4575fe7d71fc123b48
rohansah29 <rohansah1997.29@gmail.com>
2023-07-18 15:38:25 +0530

    base_file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>"Welcome to our remarkable tech team where innovation knows no limits!"</h1>
+    </div>
+  );
+}
+
+export default App;
