a72fcc3b4f9a33997d3d385a7eb0042f25ef0a17
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-07-20 23:00:01 +0530

    homepage part-1

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Box, Button, Flex, Heading, Image, VStack } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import purplebg from '../Images/purplebg.jpg'
+import 'aos/dist/aos.css';
+import Aos from "aos";
+import {GiReceiveMoney} from 'react-icons/gi'
 
 const Homepage = () => {
+
+  useEffect(()=>{
+    Aos.init()
+  },[])
   return (
-    <div>Homepage</div>
+    <Box bgImage={purplebg} pt="50px">
+      <Flex align={"center"} m="auto" justify="space-around" w="90%">
+          
+          <VStack w="600px" textAlign={"left"} spacing="30px" >
+            <Heading fontSize={"50px"} color={"white"}>Compare Loans From Several Banks Find The Cheapest Loan</Heading>
+            <Button rightIcon={<GiReceiveMoney size="25px" />} colorScheme={"gray"} borderRadius={"20px"} size={"lg"} >Apply Now</Button>
+
+          </VStack>
+
+        <Box data-aos="fade-left">
+
+      <Image src={"https://html-template.spider-themes.net/banca/img/banner/person-2.png"} />
+        </Box>
+      </Flex>
+    </Box>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/provider";
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { extendTheme } from "@chakra-ui/react";
+
+const breakpoints = {
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+};
+
+const theme = extendTheme({ breakpoints });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <ChakraProvider>
+  <ChakraProvider>
+    <BrowserRouter theme={theme}>
       <App />
-    </ChakraProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
