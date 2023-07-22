3a5f1cfec444bd8105dc86cb09521e732b555e59
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-07-22 22:22:39 +0530

    homepage part-5, part-6

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,7 +1,13 @@
 import {
   Box,
   Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
   Flex,
+  Grid,
+  GridItem,
   Heading,
   Highlight,
   HStack,
@@ -9,6 +15,7 @@ import {
   List,
   ListIcon,
   ListItem,
+  Stack,
   Step,
   StepDescription,
   StepIcon,
@@ -32,6 +39,24 @@ import styled, { keyframes } from "styled-components";
 import family_2 from "../Images/family_2.jpg";
 import family_1 from "../Images/family_1.jpg";
 import { BsFillCheckCircleFill } from "react-icons/bs";
+import article_1 from "../Images/article_1.png";
+import article_2 from "../Images/article_2.png";
+import article_3 from "../Images/article_3.png";
+import article_4 from "../Images/article_4.png";
+import { SlCalender } from "react-icons/sl";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { Banks } from "../Images/Banks";
+import dena_bank from "../Images/dena_bank.jpg";
+import federal_bank from "../Images/federal_bank.png";
+import hdfc from "../Images/hdfc.jpg";
+import hsbc from "../Images/hsbc.png";
+import ICICI from "../Images/ICICI.jpg";
+import idbi from "../Images/idbi.jpg";
+import indian_bank from "../Images/indian_bank.jpg";
+import indusind from "../Images/indusind.png";
+import karnataka_bank from "../Images/karnataka_bank.jpg";
+import kotak from "../Images/kotak.png";
 
 const steps = [
   { title: "Apply free of charge & without obligation", description: " " },
@@ -50,6 +75,7 @@ const Homepage = () => {
   });
   return (
     <Box>
+      {/* Part-1 main preview -------------------------------------------------------------------- */}
       <Flex
         p="20px"
         bgImage={purplebg}
@@ -118,7 +144,7 @@ const Homepage = () => {
           <FloatingImage src={money} />
         </Box>
       </Flex>
-      {/* part-2 ------------------------------------------------------- */}
+      {/* part-2 steeper ------------------------------------------------------- */}
 
       <VStack
         w={{
@@ -143,6 +169,7 @@ const Homepage = () => {
             lg: "40px",
             xl: "40px",
           }}
+          color="black"
         >
           <Highlight
             query={["The", "Cheapest"]}
@@ -257,9 +284,9 @@ const Homepage = () => {
         </Box>
       </Flex>
 
-      {/* Part-3--------------------------------------------------------------------- */}
+      {/* Part-3 family--------------------------------------------------------------------- */}
 
-      <Box m="auto" mt="150px">
+      <Box m="auto" mt="150px" bgColor={"#eff7f9"} p="10px">
         <Heading
           mb="70px"
           fontSize={{
@@ -270,10 +297,11 @@ const Homepage = () => {
             xl: "40px",
           }}
           data-aos="zoom-in"
+          color="black"
         >
           We have Helped More Than 10,000 Users
         </Heading>
- {/* image-1--------------------------------- */}
+        {/* image-1--------------------------------- */}
         <Box
           data-aos="zoom-in"
           m="auto"
@@ -284,8 +312,13 @@ const Homepage = () => {
             lg: "800px",
             xl: "950px",
           }}
-
-          display={{base:"none", sm:"none", md:"block", lg:"block", xl:"block"}}
+          display={{
+            base: "none",
+            sm: "none",
+            md: "block",
+            lg: "block",
+            xl: "block",
+          }}
           borderRadius={"20px"}
           boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
         >
@@ -309,8 +342,13 @@ const Homepage = () => {
             // lg: "800px",
             // xl: "950px",
           }}
-
-          display={{base:"block", sm:"block", md:"none", lg:"none", xl:"none"}}
+          display={{
+            base: "block",
+            sm: "block",
+            md: "none",
+            lg: "none",
+            xl: "none",
+          }}
           borderRadius={"20px"}
           boxShadow="rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px"
         >
@@ -325,9 +363,21 @@ const Homepage = () => {
           data-aos="zoom-in"
           p="10px"
           m="auto"
-          gap={{base:"20px", sm:"20px", md:"40px", lg:"20px", xl:"20px"}}
-          justifyContent={{base:"center", sm:"center", md:"space-around", lg:"space-evenly", xl:"space-evenly" }}
-          flexDirection={{base:"column", sm:"column", md:"row", lg:"row", xl:"row"}}
+          gap={{ base: "20px", sm: "20px", md: "40px", lg: "20px", xl: "20px" }}
+          justifyContent={{
+            base: "center",
+            sm: "center",
+            md: "space-around",
+            lg: "space-evenly",
+            xl: "space-evenly",
+          }}
+          flexDirection={{
+            base: "column",
+            sm: "column",
+            md: "row",
+            lg: "row",
+            xl: "row",
+          }}
           w="80%"
         >
           <Box
@@ -398,7 +448,13 @@ const Homepage = () => {
               lg: "17px",
               xl: "17px",
             }}
-            w={{base:"300px", sm:"300px", md:"500px", lg:"550px", xl:"550px"}}
+            w={{
+              base: "300px",
+              sm: "300px",
+              md: "500px",
+              lg: "550px",
+              xl: "550px",
+            }}
             textAlign={"left"}
             color="blackAlpha.700"
           >
@@ -419,6 +475,250 @@ const Homepage = () => {
           </Box>
         </HStack>
       </Box>
+
+
+      {/* part-5 Banks -------------------------------------------------------------------------------- */}
+
+      <Box mt="150px"  bgColor={"#eff7f9"} p="20px">
+        <Heading color="black" data-aos="zoom-in-up">
+          Money Mentor Cooperates With These Banks
+        </Heading>
+
+        <Text fontSize={"22px"} mb="50px" data-aos="zoom-in-up">
+          {" "}
+          <span style={{ fontSize: "25px", fontWeight: "bold" }}>
+            100+ Banks
+          </span>{" "}
+          Trust Money Mentor to Offered{" "}
+          <span style={{ fontSize: "25px", fontWeight: "bold" }}>
+            Best Loans
+          </span>
+        </Text>
+
+        <Grid
+          templateColumns={{
+            base: "repeat(2, 1fr)",
+            sm: "repeat(2, 1fr)",
+            md: "repeat(3, 1fr)",
+            lg: "repeat(5, 1fr)",
+            xl: "repeat(5, 1fr)",
+          }}
+          w="80%"
+          m="auto"
+          alignItems={"center"}
+          placeItems="center"
+          textAlign={"center"}
+          gap="20px"
+        >
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={karnataka_bank}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+            ,
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={kotak}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={indusind}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={indian_bank}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={idbi}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={ICICI}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={hsbc}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={hdfc}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={federal_bank}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+          <Box w="150px" borderRadius="20px" data-aos="zoom-in-up">
+            <Image
+              src={dena_bank}
+              role="presentation"
+              w="100%"
+              borderRadius="20px"
+              data-aos="zoom-in-up"
+            />
+          </Box>
+          ,
+        </Grid>
+      </Box>
+
+      {/* Part-6 articles --------------------------------------------------------------------- */}
+
+      <Box mt="150px">
+        <Heading data-aos="zoom-in-up" color={"black"}>
+          More About Finance & Loans
+        </Heading>
+
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(1, 1fr)",
+            md: "repeat(2, 1fr)",
+            lg: "repeat(4, 1fr)",
+            xl: "repeat(4, 1fr)",
+          }}
+          justifyContent="center"
+          gap="30px"
+          w="80%"
+          m="auto"
+          alignItems={"center"}
+        >
+          <Card
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            _hover={{
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+            }}
+            className="card"
+            boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+          >
+            <CardBody align="center">
+              <Image src={article_1} alt="article_1" borderRadius="lg" />
+              <Stack textAlign={"left"}>
+                <Heading textAlign={"left"} color={"black"} size="md">
+                  Credit rating as a private customer
+                </Heading>
+                <Text color={"blackAlpha.700"}>📆 July 24, 2023</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+
+          <Card
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            _hover={{
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+            }}
+            boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+          >
+            <CardBody align="center">
+              <Image src={article_2} alt="article_2" borderRadius="lg" />
+              <Stack textAlign={"left"}>
+                <Heading textAlign={"left"} color={"black"} size="md">
+                  Get hold of your private loans with a promissory
+                </Heading>
+                <Text color={"blackAlpha.700"}>📆 July 24, 2023</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+
+          <Card
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            _hover={{
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+            }}
+            boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+          >
+            <CardBody align="center">
+              <Image src={article_3} alt="article_3" borderRadius="lg" />
+              <Stack textAlign={"left"}>
+                <Heading textAlign={"left"} color={"black"} size="md">
+                  What is APR and what can you actually use it?
+                </Heading>
+                <Text color={"blackAlpha.700"}>📆 July 24, 2023</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+
+          <Card
+            data-aos="fade-right"
+            data-aos-duration="1500"
+            _hover={{
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px",
+            }}
+            boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px"
+          >
+            <CardBody align="center">
+              <Image src={article_4} alt="article_4" borderRadius="lg" />
+              <Stack textAlign={"left"}>
+                <Heading textAlign={"left"} color={"black"} size="md">
+                  Superfast loans for your dream home
+                </Heading>
+                <Text color={"blackAlpha.700"}>📆 July 24, 2023</Text>
+              </Stack>
+            </CardBody>
+          </Card>
+        </Grid>
+      </Box>
+
+      
     </Box>
   );
 };
