94e5cef25a9643b01b72ca02ce491a618e3aae61
Nitesh <niteshchandrakar2@gmail.com>
2023-07-23 23:33:20 +0530

    fw25_034

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,8 +2,12 @@ import { Button } from "@chakra-ui/react"
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import { useEffect,useState } from "react"
+import { Box } from "@chakra-ui/react"
+import { Text } from "@chakra-ui/react"
 export const Profile=()=>{
-    
+    const loans=[{category:"HomeLoan",image:"https://personalloan.paisabazaar.com/components/images/bank-new-logos/93.gif",amount:1000},
+    {category:"HomeLoan",image:"https://personalloan.paisabazaar.com/components/images/bank-new-logos/93.gif",amount:1000}
+]
     const data=useSelector((store)=>
     store.authReducer.currentUser
     )
@@ -18,12 +22,12 @@ export const Profile=()=>{
       return () => window.removeEventListener("resize", updateMedia);
     });
     return (
-        <div style={{display:window.innerWidth>650?"flex":"grid",justifyContent:"start",paddingTop:"20px",backgroundColor:"#E5D1FA"}}>
-            <div>
+        <div style={{display:window.innerWidth>650?"flex":"grid",justifyContent:"start",paddingTop:"20px",backgroundColor:"#E5D1FA",alignContent:"center"}}>
+            <div style={{display:"flex",alignContent:"center",alignItems:"center",justifyContent:"center"}}>
         
-            <img style={{ width: "70%",paddingRight:"50px",paddingLeft:"10%" }} src="https://cdn-icons-png.flaticon.com/512/6681/6681204.png" alt="" />
+            <img style={{ width: "70%",paddingRight:"50px",paddingLeft:"10%" }} src="https://img.freepik.com/premium-vector/man-profile-cartoon_18591-58482.jpg?w=740" alt="" />
             </div>
-            <div style={{marginLeft:"10%"}}>
+            <div style={{marginLeft:"10%",paddingBottom:"10px"}}>
                 <h3> <label style={{color:"#07051f"}}>Name :  </label> {data.fullname} {data.lastname}</h3>
                 <h5> <label style={{color:"#07051f"}}>Birth :  </label> {data.dob}</h5>
                 <h5> <label style={{color:"#07051f"}}>Phone :  </label> {data.contact}</h5>
@@ -32,11 +36,26 @@ export const Profile=()=>{
                 <h5> <label style={{color:"#07051f"}}>Income :  </label> {data.income}</h5>
                 <h5> <label style={{color:"#07051f"}}>Town :  </label> {data.address}</h5>
                 <h5> <label style={{color:"#07051f"}}>Occupation: </label> {data.employment}</h5>
-                <Button>Edit Password</Button>
+               
             </div>
             <div style={{marginLeft:"10%"}}>
-               {data.loans.length>0?(
-               ""
+               {loans.length>0?(
+                
+               <div >
+                <Text fontSize={"15px"} fontWeight={"bold"}>Active Loans</Text>
+                {loans.map((el)=>(
+                <Box pr={"10px"} mb={"10px"} >
+                  <Box>  <img src={el.image} alt="" /></Box>
+                  <Box style={{display:"flex",justifyContent:"space-around",width:"250px"}}>
+                 <Text fontSize={"15px"} fontWeight={"bold"}>{el.category}</Text>
+                 <Text fontSize={"15px"} fontWeight={"bold"}>Loan: Rs{el.amount}</Text>
+                 
+                  </Box>
+                </Box>
+
+
+                ))}
+               </div>
                ):(
                 <div>
                     <h1 style={{fontWeight:"bold",paddingTop:"20px",paddingRight:"20px"}}>! You have not Taken any Loans</h1>
